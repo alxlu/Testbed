e7582b0ea0454b1076c3220478628497740b099a
Alex Lu <alxlu@users.noreply.github.com>
2015-07-18 15:23:57 -0700

    split up tests

--- test/test.js
+++ test/test.js
@@ -18,94 +18,18 @@ exec('node ' + gulppath + ' build', function() {
 
 describe('Win 10 Testbed E2E', function() {
   this.timeout(99999999);
-  var client = {};
+  this.client = {};
 
   before(function(done) {
-    client = webdriverio.remote({desiredCapabilities: {browserName: 'chrome'} });
-    client.init(done);
+    this.client = webdriverio.remote({desiredCapabilities: {browserName: 'chrome'} });
+    this.client.init(done);
   });
 
-  describe('navigating to sites', function() {
-    it('loads the site', function(done) {
-      client
-      .url('http://localhost:8080')
-      .waitForExist('body', SITE_LOAD_TIMEOUT)
-      .getTitle().then(function(title) {
-        expect(title).to.equal('Windows 10 Testbed');
-      })
-      .call(done);
-    });
-
-    it('navigates to ContosoTravel using the Go button', function(done) {
-      client
-      .click('#server-address-input')
-      .keys('https://contosotravel.azurewebsites.net')
-      .click('#submit-address')
-      .waitUntil(function() {
-        return this.getTitle().then(function(title) {
-          return title && title.indexOf && title.indexOf('contoso') > -1;
-        });
-      }, SITE_LOAD_TIMEOUT)
-      .url(function(err, res) {
-        expect(res.value).to.equal('https://contosotravel.azurewebsites.net/');
-      })
-      .call(done);
-    });
-
-    it('navigates back to the testbed and saves the previously visited url(s)', function(done) {
-      client
-      .back()
-      .pause(300)
-      .waitForExist('body', SITE_LOAD_TIMEOUT)
-      .pause(2000)
-      .call(done);
-    });
-
-    it ('navigates to RJS page using enter key', function(done) {
-      client
-        .click('#server-address-input')
-        .keys('https://rjs.azurewebsites.net')
-        .keys('Enter')
-        .waitUntil(function() {
-          return this.getTitle().then(function(title) {
-            return title && title.indexOf && title.indexOf('rjs') > -1;
-          });
-        }, SITE_LOAD_TIMEOUT)
-        .url(function(err, res) {
-          expect(res.value).to.equal('https://rjs.azurewebsites.net/');
-        })
-        .call(done);
-    });
-
-    it('navigates back to the testbed and saves the previously visited url(s)', function(done) {
-      client
-      .back()
-      .pause(300)
-      .waitForExist('body', SITE_LOAD_TIMEOUT)
-      .pause(2000)
-      .call(done);
-    });
-
-    it ('navigates to bing', function(done) {
-      client
-      .click('#server-address-input')
-      .keys('http://bing.com')
-      .click('#submit-address')
-      .waitUntil(function() {
-        return this.getTitle().then(function(title) {
-          return (title && title.indexOf) && title.indexOf('ing') > -1;
-        });
-      }, SITE_LOAD_TIMEOUT)
-      .url(function(err, res) {
-        expect(res.value).to.contain('bing');
-      })
-      .call(done);
-    });
-  });
+  describe('navigating to sites', require('./webdriver/navigateTest').bind(this));
 
   describe('checking added sites', function() {
     it ('keeps the list of sites stored', function(done) {
-      client
+      this.client
       .url('http://localhost:8080')
       .pause(3000)
       .call(done);
@@ -116,7 +40,7 @@ describe('Win 10 Testbed E2E', function() {
   });
 
   after(function(done) {
-    client.end(done);
+    this.client.end(done);
   });
 
 });
---
+++ test/webdriver/navigateTest.js
@@ -0,0 +1,82 @@
+var expect = require('chai').expect;
+var SITE_LOAD_TIMEOUT = 30000;
+
+var navigateTest = function() {
+  it('loads the site', function(done) {
+    this.client
+    .url('http://localhost:8080')
+    .waitForExist('body', SITE_LOAD_TIMEOUT)
+    .getTitle().then(function(title) {
+      expect(title).to.equal('Windows 10 Testbed');
+    })
+    .call(done);
+  });
+
+  it('navigates to ContosoTravel using the Go button', function(done) {
+    this.client
+    .click('#server-address-input')
+    .keys('https://contosotravel.azurewebsites.net')
+    .click('#submit-address')
+    .waitUntil(function() {
+      return this.getTitle().then(function(title) {
+        return title && title.indexOf && title.indexOf('contoso') > -1;
+      });
+    }, SITE_LOAD_TIMEOUT)
+    .url(function(err, res) {
+      expect(res.value).to.equal('https://contosotravel.azurewebsites.net/');
+    })
+    .call(done);
+  });
+
+  it('navigates back to the testbed and saves the previously visited url(s)', function(done) {
+    this.client
+    .back()
+    .pause(300)
+    .waitForExist('body', SITE_LOAD_TIMEOUT)
+    .pause(2000)
+    .call(done);
+  });
+
+  it ('navigates to RJS page using enter key', function(done) {
+    this.client
+    .click('#server-address-input')
+    .keys('https://rjs.azurewebsites.net')
+    .keys('Enter')
+    .waitUntil(function() {
+      return this.getTitle().then(function(title) {
+        return title && title.indexOf && title.indexOf('rjs') > -1;
+      });
+    }, SITE_LOAD_TIMEOUT)
+    .url(function(err, res) {
+      expect(res.value).to.equal('https://rjs.azurewebsites.net/');
+    })
+    .call(done);
+  });
+
+  it('navigates back to the testbed and saves the previously visited url(s)', function(done) {
+    this.client
+    .back()
+    .pause(300)
+    .waitForExist('body', SITE_LOAD_TIMEOUT)
+    .pause(2000)
+    .call(done);
+  });
+
+  it ('navigates to bing', function(done) {
+    this.client
+    .click('#server-address-input')
+    .keys('http://bing.com')
+    .click('#submit-address')
+    .waitUntil(function() {
+      return this.getTitle().then(function(title) {
+        return (title && title.indexOf) && title.indexOf('ing') > -1;
+      });
+    }, SITE_LOAD_TIMEOUT)
+    .url(function(err, res) {
+      expect(res.value).to.contain('bing');
+    })
+    .call(done);
+  });
+};
+
+module.exports = navigateTest;
